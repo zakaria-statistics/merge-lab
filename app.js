bf92958e0db53409d8182fa2f8839ed5641a93fe
Zakaria EL MOUMNAOUI <zakariaelmoumnaoui96@gmail.com>
2024-12-24 15:28:50 +0100

    added addNumber and subNumber

---
+++ app.js
@@ -0,0 +1,13 @@
+function addnumbers(a,b){
+    return a+b;
+}
+
+console.log(addnumbers(2,3));
+
+function subnumbers(a,b){
+    return a-b;
+    // This line won't be executed
+}
+
+console.log('This is line');
+console.log(subnumbers(2,3));
